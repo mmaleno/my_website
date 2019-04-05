25e8a77b12f1efa220f2f63ac3f8461a6e36c8a6
mmaleno <mmaleno@hmc.edu>
2019-04-05 08:55:24 +0000

    added multiple subpages, added html comments

--- transfer.sh
+++ transfer.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 sudo cp index.html /var/www/html/index.html
+
+sudo cp about.html /var/www/html/about.html
+
+sudo cp projects.html /var/www/html/projects.html
+
+sudo cp photos.html /var/www/html/photos.html
